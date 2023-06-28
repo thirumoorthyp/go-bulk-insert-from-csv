a341fc296bca5d818e47f7143e69d5ad48576605
Thirumoorthy.P <thirumoorthy.p@payoda.com>
2023-06-28 21:41:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# go-bul-insert-from-csv-into-sqlite
+This go-lang code is used to bulk import data from CSV file into the SQLite database.
+
+# Manual Steps: 
+``` shell
+apt install golang
+```
+
+``` shell
+go version
+```
+# go version go1.20.3
+
+# config
+``` shell
+ set GOOS=linux
+ set GOARCH=amd64
+ set CGO_ENABLED=0
+```
+
+# install sqlite
+``` shell
+go get github.com/modernc.org/sqlite
+```
+
+# executing go program
+```shell
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+
+	start := time.Now()
+	bulkInsert()
+	//Calculating total execution time
+	elapsed := time.Since(start)
+	fmt.Printf("The time of execution of Go program is : %s", elapsed)
+}
+
+// To get SQLite connection
+func getSQLiteDB() (*sql.DB, error) {
+	x, err := sql.Open("sqlite", "./sqlite_employees.db")
+	if err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+// Function helps to insert data in bulk
+func bulkInsert() error {
+	x, err := getSQLiteDB()
+	if err != nil {
+		return err
+	}
+	defer x.Close()
+	records, err := loadCSV()
+	if err != nil {
+		return err
+	}
+	tx, err := x.Begin()
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewBuffer([]byte("INSERT INTO employee_details (emp_id, emp_name, emp_dob, emp_role, emp_dept) VALUES "))
+	for i := 0; i < 249; i++ {
+		if i > 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteString("(?,?,?,?,?)")
+	}
+	bulkInsertStmt, err := tx.Prepare(buf.String())
+	if err != nil {
+		return err
+	}
+	var values []interface{}
+	for _, record := range records {
+
+		emp_id, _ := strconv.Atoi(record[0])
+		values = append(values, emp_id)
+		values = append(values, record[1])
+		values = append(values, record[2])
+		values = append(values, record[3])
+		values = append(values, record[4])
+		if len(values) == 996 {
+			if _, err = bulkInsertStmt.Exec(values...); err != nil {
+				return err
+			}
+			values = values[0:0]
+		}
+	}
+	if len(values) > 0 {
+		buf := bytes.NewBuffer([]byte("INSERT INTO employee_details (emp_id, emp_name, emp_dob, emp_role, emp_dept) VALUES "))
+		for i := 0; i < len(values)/5; i++ {
+			if i > 0 {
+				buf.WriteString(",")
+			}
+			buf.WriteString("(?,?,?,?, ?)")
+		}
+		if _, err = tx.Exec(buf.String(), values...); err != nil {
+			return err
+		}
+	}
+	return tx.Commit()
+}
+
+func loadCSV() ([][]string, error) {
+	f, err := os.Open("employees.csv")
+	if err != nil {
+		return nil, err
+	}
+	r := csv.NewReader(f)
+	return r.ReadAll()
+}
